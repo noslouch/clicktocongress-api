b58ce7fdea4fa768697a77871e6ca6dba3ab39e6
Brian Whitton <me@brianwhitton.com>
2016-11-17 21:57:48 -0500

    do not need a leading +

--- web/controllers/call_controller.ex
+++ web/controllers/call_controller.ex
@@ -2,8 +2,8 @@ defmodule Clicktocongress.CallController do
   use Clicktocongress.Web, :controller
   
   def call_me(conn, %{"dial_out" => dial_out, "my_number" => to}) do
-    {:ok, _} = ExTwilio.Call.create url: "http://efc56f1f.ngrok.io/api/connect?t=+#{dial_out}",
-                         to: "+#{to}",
+    {:ok, _} = ExTwilio.Call.create url: "http://eb52c591.ngrok.io/api/connect?t=#{dial_out}",
+                         to: to,
                          from: System.get_env "TWILIO_NUMBER"
     text conn, :ok
   end
