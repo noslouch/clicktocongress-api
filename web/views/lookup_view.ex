b17479c3618c7fc2f806f94f264efcb4c4b8fcb8
Brian Whitton <me@brianwhitton.com>
2016-11-21 01:20:47 -0500

    adds district and bioguide_id to congressional lookups

--- web/views/lookup_view.ex
+++ web/views/lookup_view.ex
@@ -1,7 +1,7 @@
 defmodule Clicktocongress.LookupView do
   use Clicktocongress.Web, :view
   
-  @keys ~w{first_name last_name phone chamber}
+  @keys ~w{first_name last_name phone title party district bioguide_id}
   
   def render("lookup.json", %{data: data}) do
     data
