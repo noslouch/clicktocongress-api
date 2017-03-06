ff0e4452ff40fc421b48dd583bc8a1bfdd36bbe5
Brian Whitton <me@brianwhitton.com>
2017-03-05 22:38:23 -0500

    refactor lookup controller to lookup zip locally before calling google

--- web/controllers/lookup_controller.ex
+++ web/controllers/lookup_controller.ex
@@ -1,9 +1,18 @@
 defmodule Clicktocongress.LookupController do
+  alias Clicktocongress.Zip
+  
   use Clicktocongress.Web, :controller
   
   def lookup(conn, %{"zip" => zip}) do
-    params = GoogleMaps.lookup_zip(zip)
-    lookup(conn, params)
+    case Repo.get_by Zip, address: zip do
+      nil -> 
+        results = GoogleMaps.lookup_zip(zip)
+        Zip.changeset(%Zip{}, Map.merge(%{"address" => zip}, results))
+          |> Repo.insert
+        lookup(conn, results)
+      %{latitude: lat, longitude: long} ->
+        lookup(conn, %{"latitude" => lat, "longitude" => long})
+    end
   end
   
   def lookup(conn, params) do
